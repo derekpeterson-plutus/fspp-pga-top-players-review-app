df3fbecba6daec687a66cce94e3798aff7eb8e52
Derek <plutus.derekpeterson@gmail.com>
2022-08-25 13:25:29 -0400

    heroku deployment

---
+++ back-end/db/prod_schema.sql
@@ -0,0 +1,19 @@
+-- CONNECTING TO THE DATABASE
+\c d9ofgkv6951eiu; 
+
+DROP TABLE IF EXISTS players;
+
+-- CREATING THE SNACKS TABLE
+CREATE TABLE players (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    rank_this_week INT DEFAULT 0,
+    rank_last_week INT DEFAULT 0,
+    country TEXT NOT NULL,
+    events INT DEFAULT 0,
+    avg_points DECIMAL,
+    total_points DECIMAL,
+    money_earned TEXT NOT NULL,
+    is_favorite BOOLEAN,
+    image TEXT NOT NULL
+);
