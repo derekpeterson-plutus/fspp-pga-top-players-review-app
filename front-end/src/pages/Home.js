ff52067140cd09da549ab5d11ade68957841656d
Derek <plutus.derekpeterson@gmail.com>
2023-01-30 19:15:50 -0500

    Add max-width to Players.js

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,6 +1,6 @@
 import './Home.scss';
 import axios from 'axios';
-import { FaPlus, FaMinus } from 'react-icons/fa';
+//import { FaPlus, FaMinus } from 'react-icons/fa';
 import { Link, useParams } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 
@@ -9,7 +9,7 @@ const API = process.env.REACT_APP_API_URL;
 const Home = () => {
   const { id } = useParams();
   const [players, setPlayers] = useState([]);
-  const [showPlayerDetails, setShowPlayerDetails] = useState(false);
+  //const [showPlayerDetails, setShowPlayerDetails] = useState(false);
 
   useEffect(() => {
     axios
@@ -22,21 +22,21 @@ const Home = () => {
       });
   }, [id]);
 
-  const togglePlayerDetails = () => {
-    !showPlayerDetails
-      ? setShowPlayerDetails(true)
-      : setShowPlayerDetails(false);
-  };
+  // const togglePlayerDetails = () => {
+  //   !showPlayerDetails
+  //     ? setShowPlayerDetails(true)
+  //     : setShowPlayerDetails(false);
+  // };
 
   return (
-    <div className='homePlayers'>
-      <div className='homePlayers__container'>
-        {players.map((player, id) => {
-          return (
-            <div key={id} className='homePlayers__details'>
+    // <div className='homePlayers'>
+    //   <div className='homePlayers__container'>
+    //     {players.map((player, id) => {
+    //       return (
+            {/* <div key={id} className='homePlayers__details'>
               <Link key={id} to={`/players/${player.id}`}>
                 <img alt={player.name} src={player.image} title={player.name} />
-              </Link>
+              </Link> 
               <div className='homePlayers__playerInfo'>
                 <div className='text-xl font-bold'>{player.name}</div>
                 <div className='homePlayers__player-country'>
@@ -60,12 +60,22 @@ const Home = () => {
                   />
                 )}
               </div>
-            </div>
-          );
-        })}
+            </div> */},
+    <div className='home'>
+      <div className='home__left'>
+        <div className='home__hero'>
+          <img alt='' src="https://www.golfchannel.com/sites/default/files/2023/01/28/homa_1920_farmers23_D4_trophy.jpg"/>
+          <span className='home__leftInfo'>Hope springs Max Homa to new heights at Torrey Pines</span>
+        </div>
+  
       </div>
+     
     </div>
-  );
+  //         );
+  //       })}
+  //     </div>
+  //   </div>
+ );
 };
 
 export default Home;
