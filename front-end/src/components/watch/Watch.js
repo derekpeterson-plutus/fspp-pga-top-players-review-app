6170a27508357f41c5fababcefa638bc21a74dc2
Derek <plutus.derekpeterson@gmail.com>
2023-02-04 00:48:38 -0500

    Add background image to Watch.js component

--- front-end/src/components/watch/Watch.js
+++ front-end/src/components/watch/Watch.js
@@ -1,10 +1,13 @@
 import './Watch.scss';
 import React from 'react';
+import bgImg3 from '../../assets/bgImg3.webp';
 
 const Watch = () => {
   return (
-    <div>
-      <div>Watch your favorite golfers here!</div>
+    <div className='watch'>
+      <div className='watch__container'>
+        <img alt="bgImg" src={bgImg3}/>
+      </div>
     </div>
   )
 };
