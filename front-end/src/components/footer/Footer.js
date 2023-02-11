e5b939486f8cd610df84f92cd2f187d0c1fe6a00
Derek <plutus.derekpeterson@gmail.com>
2023-02-11 15:03:39 -0500

    Give each footer icon a title

--- front-end/src/components/footer/Footer.js
+++ front-end/src/components/footer/Footer.js
@@ -21,29 +21,29 @@ const Footer = () => {
             <div className='footer__link'><Link to='/privacy'>Privacy</Link></div>
         </div>
         <div className='footer__icons'>
-            <div className='footer__icon'>
+            <div className='footer__icon' title='GitHub'>
                 <a href='https://www.github.com/derekpeterson-plutus' target='_blank' rel='noopener noreferrer'><BsGithub size={25} /></a>
             </div>
-            <div className='footer__icon'>
-                <a href='https://www.linkedin.com/in/derekpeterson-plutus' target='_blank' rel='noopener noreferrer'><FaLinkedinIn size= {25}/></a>
+            <div className='footer__icon' title='LinkedIn'>
+                <a href='https://www.linkedin.com/in/derekpeterson-plutus' target='_blank' rel='noopener noreferrer'><FaLinkedinIn size= {25} /></a>
             </div>
-            <div className='footer__icon'>
-                <a href='https://www.twitter.com/plutusmercurius' target='_blank' rel='noopener noreferrer'><BsTwitter size={25}/></a>
+            <div className='footer__icon' title='Twitter'>
+                <a href='https://www.twitter.com/plutusmercurius' target='_blank' rel='noopener noreferrer'><BsTwitter size={25} /></a>
             </div>
-            <div className='footer__icon'>
-                <a href='https://www.gmail.com/' target='_blank' rel='noopener noreferrer'><FaEnvelope size={25}/></a>
+            <div className='footer__icon' title='Email'>
+                <a href='https://www.gmail.com/' target='_blank' rel='noopener noreferrer'><FaEnvelope size={25} /></a>
             </div> 
-            <div className='footer__icon'>
-                <a href='https://www.instagram.com/pgatour/' target='_blank' rel='noopener noreferrer'><BsInstagram size={25}/></a>
+            <div className='footer__icon' title='Instagram'>
+                <a href='https://www.instagram.com/pgatour/' target='_blank' rel='noopener noreferrer'><BsInstagram size={25} /></a>
             </div>
-            <div className='footer__icon'>
-                <a href='https://www.facebook.com/pgatour' target='_blank' rel='noopener noreferrer'><FaFacebookF size={25}/></a>
+            <div className='footer__icon' title='Facebook'>
+                <a href='https://www.facebook.com/pgatour' target='_blank' rel='noopener noreferrer'><FaFacebookF size={25} /></a>
             </div>
-            <div className='footer__icon'>
-                <a href='https://www.tiktok.com/@pgatour' target='_blank' rel='noopener noreferrer'><FaTiktok size={23}/></a>
+            <div className='footer__icon' title='Tiktok'>
+                <a href='https://www.tiktok.com/@pgatour' target='_blank' rel='noopener noreferrer'><FaTiktok size={25} /></a>
             </div>
-            <div className='footer__icon'>
-                <a href='https://www.youtube.com/pgatour' target='_blank' rel='noopener noreferrer'><BsYoutube size={26}/></a>
+            <div className='footer__icon' title='YouTube'>
+                <a href='https://www.youtube.com/pgatour' target='_blank' rel='noopener noreferrer'><BsYoutube size={25} /></a>
             </div> 
         </div>
         <div className='footer__copyright'>
