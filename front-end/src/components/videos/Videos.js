292031f71c39ea8254f24bd0cd51ffa201af1730
Derek <plutus.derekpeterson@gmail.com>
2023-03-01 23:30:09 -0500

    Add media quries to home page

--- front-end/src/components/videos/Videos.js
+++ front-end/src/components/videos/Videos.js
@@ -1,13 +1,14 @@
 import './Videos.scss';
 import React from 'react';
 //import video from '../../assets/video.mp4'
-//import VideoPlayer from '../videoPlayer/VideoPlayer';
+import VideoPlayer from '../videoPlayer/VideoPlayer';
 
 const Videos = () => {
   return (
     <div className='videos'>
+      <div className="videos__content">
+      <div className='videos__title'>Videos</div>
       <div className='videos__container'>
-        {/* <VideoPlayer />
         <VideoPlayer />
         <VideoPlayer />
         <VideoPlayer />
@@ -15,7 +16,8 @@ const Videos = () => {
         <VideoPlayer />
         <VideoPlayer />
         <VideoPlayer />
-        <VideoPlayer /> */}
+        <VideoPlayer />
+        <VideoPlayer />
 
         {/* <div>
           <video src={video} width="750" height="500" controls></video>
@@ -29,6 +31,7 @@ const Videos = () => {
         <div>
           <video src={video} width="750" height="500" controls></video>
         </div> */}
+      </div>
       </div>
     </div>
   )
